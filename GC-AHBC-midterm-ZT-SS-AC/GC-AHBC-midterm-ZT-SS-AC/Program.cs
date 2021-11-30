81dfca0b0a43c7330b5d363b2e76c21c428045f3
ztata <zacharyptata@gmail.com>
2021-11-29 19:20:35 -0500

    initial commit

---
+++ GC-AHBC-midterm-ZT-SS-AC/GC-AHBC-midterm-ZT-SS-AC/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GC_AHBC_midterm_ZT_SS_AC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
