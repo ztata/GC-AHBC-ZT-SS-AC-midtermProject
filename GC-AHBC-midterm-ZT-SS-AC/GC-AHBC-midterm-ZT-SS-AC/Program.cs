5067dda9757092718f8cabe5dcb7842fbd38431f
ztata <zacharyptata@gmail.com>
2021-11-29 21:43:18 -0500

    commit note

--- GC-AHBC-midterm-ZT-SS-AC/GC-AHBC-midterm-ZT-SS-AC/Program.cs
+++ GC-AHBC-midterm-ZT-SS-AC/GC-AHBC-midterm-ZT-SS-AC/Program.cs
@@ -12,8 +12,73 @@ namespace GC_AHBC_midterm_ZT_SS_AC
 
             FileHelper.BuildMenuList(addressPath);
 
-            // Adds the created product to the file
-            FileHelper.AddProductToFile(addressPath, product);
+
+                //bool value that allows them to loop and order another item 
+                bool orderAnotherItem = true;
+                while (orderAnotherItem)
+                {
+                    Console.WriteLine("Please enter the number preceeding the item you would like to order: ");
+                    userInput = Console.ReadLine();
+                    int userChoice = -1;
+                    try
+                    {
+                        userChoice = int.Parse(userInput);
+                    }
+                    catch (FormatException)
+                    {
+                        Console.WriteLine("Sorry, that is not a valid input!");
+                        continue;
+                    }
+
+                    if (userChoice < 1 || userChoice > 12)
+                    {
+                        Console.WriteLine("Sorry, your input needs to be between 1 and 12!");
+                        continue;
+                    }
+                    else
+                    {
+                        Menu userMenuSelection = (Menu) userChoice;
+                    }
+
+                    
+
+
+                }
+
+
+
+
+                TryAgain:
+                Console.WriteLine("Would you like to repeat the program?");
+                Console.Write("Enter y to coninue or anything else to quit: ");
+                userInput = Console.ReadLine();
+                if (userInput.Trim().ToLower() == "y")
+                {
+                    runProgramAgain = true;
+                }
+                else
+                {
+                    runProgramAgain = false;
+                }
+
+
+
+
+            } while (runProgramAgain);
+
+            Exit:
+            Console.WriteLine("Thank you for shopping at Jitters Coffee House!");
+            Console.WriteLine("Please press any key to continue: ");
+            Console.ReadKey();
+
+
+
+
+
+
+
+
+
         }
     }
 }
