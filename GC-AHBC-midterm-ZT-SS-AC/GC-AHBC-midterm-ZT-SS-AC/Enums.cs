bf512804a04f24c36661a0bd74e886e95c4731a5
ztata <zacharyptata@gmail.com>
2021-11-29 20:44:51 -0500

    beginning of V1 of UI

---
+++ GC-AHBC-midterm-ZT-SS-AC/GC-AHBC-midterm-ZT-SS-AC/Enums.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GC_AHBC_midterm_ZT_SS_AC
+{
+    public enum Menu
+    {
+        menuItem1 = 1,
+        menuItem2,
+        menuItem3,
+        menuItem4,
+        menuItem5,
+        menuItem6,
+        menuItem7,
+        menuItem8,
+        menuItem9,
+        menuItem10,
+        menuItem11,
+        menuItem12
+    }
+
+   
+}
--- GC-AHBC-midterm-ZT-SS-AC/GC-AHBC-midterm-ZT-SS-AC/Program.cs
+++ GC-AHBC-midterm-ZT-SS-AC/GC-AHBC-midterm-ZT-SS-AC/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace GC_AHBC_midterm_ZT_SS_AC
 {
@@ -6,8 +7,80 @@ namespace GC_AHBC_midterm_ZT_SS_AC
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            bool runProgramAgain = true;
+            string userInput = "";
+
+            do//repeats the program if the user chooses
+            {
+                Console.WriteLine("Hello and welcome to Jitters Coffee House!");
+                Menu:
+                //displays the menu and prices for items available at the shop 
+                Console.WriteLine("Please see the menu below: ");
+                Console.WriteLine("-------------------------------");
+                Console.WriteLine($"{Menu.menuItem1}). Menu Item 1 -- Price");
+                Console.WriteLine($"{Menu.menuItem2}). Menu Item 2 -- Price");
+                Console.WriteLine($"{Menu.menuItem3}). Menu Item 3 -- Price");
+                Console.WriteLine($"{Menu.menuItem4}). Menu Item 4 -- Price");
+                Console.WriteLine($"{Menu.menuItem5}). Menu Item 5 -- Price");
+                Console.WriteLine($"{Menu.menuItem6}). Menu Item 6 -- Price");
+                Console.WriteLine($"{Menu.menuItem7}). Menu Item 7 -- Price");
+                Console.WriteLine($"{Menu.menuItem8}). Menu Item 8 -- Price");
+                Console.WriteLine($"{Menu.menuItem9}). Menu Item 9 -- Price");
+                Console.WriteLine($"{Menu.menuItem10}). Menu Item 10 -- Price");
+                Console.WriteLine($"{Menu.menuItem11}). Menu Item 11 -- Price");
+                Console.WriteLine($"{Menu.menuItem12}). Menu Item 12 -- Price");
+
+                //list of items the patron has ordered 
+                List<ImenuItem> currentOrderList = new List<ImenuItem>();
+
+
+                //bool value that allows them to loop and order another item 
+                bool orderAnotherItem = true;
+                while (orderAnotherItem)
+                {
+                    Console.WriteLine("Please enter the number preceeding the item you would like to order: ");
+
+
+
+
+                }
+
+
+
+
+                TryAgain:
+                Console.WriteLine("Would you like to repeat the program?");
+                Console.Write("Enter y to coninue or anything else to quit: ");
+                userInput = Console.ReadLine();
+                if (userInput.Trim().ToLower() == "y")
+                {
+                    runProgramAgain = true;
+                }
+                else
+                {
+                    runProgramAgain = false;
+                }
+
+
+
+
+            } while (runProgramAgain);
+
+            Exit:
+            Console.WriteLine("Thank you for shopping at Jitters Coffee House!");
+            Console.WriteLine("Please press any key to continue: ");
+            Console.ReadKey();
+
+
+
+
+
+
+
+
 
         }
+
+        
     }
 }
