5b740ba5d9234b46a78ecbbc082f5708f8092497
ztata <zacharyptata@gmail.com>
2021-11-29 23:54:07 -0500

    Unified Zack, Main, and Steve

--- GC-AHBC-midterm-ZT-SS-AC/GC-AHBC-midterm-ZT-SS-AC/Program.cs
+++ GC-AHBC-midterm-ZT-SS-AC/GC-AHBC-midterm-ZT-SS-AC/Program.cs
@@ -41,14 +41,14 @@ namespace GC_AHBC_midterm_ZT_SS_AC
 
 
                 //bool value that allows them to loop and order another item 
-                //bool value that allows them to loop and order another item 
                 bool orderAnotherItem = true;
-                while (orderAnotherItem)
+                Menu userMenuSelection;
+                while (orderAnotherItem) //while loop repeats while they want to keep ordering 
                 {
                     Console.WriteLine("Please enter the number preceeding the item you would like to order: ");
                     userInput = Console.ReadLine();
                     int userChoice = -1;
-                    try
+                    try //try catch makes sure input is a integer number. will repeat loop if it doesnt work
                     {
                         userChoice = int.Parse(userInput);
                     }
@@ -58,25 +58,54 @@ namespace GC_AHBC_midterm_ZT_SS_AC
                         continue;
                     }
 
-                    if (userChoice < 1 || userChoice > 12)
+                    if (userChoice < 1 || userChoice > 12) //makes sure it is a valid number for enum selection
                     {
                         Console.WriteLine("Sorry, your input needs to be between 1 and 12!");
                         continue;
                     }
                     else
                     {
-                        Menu userMenuSelection = (Menu) userChoice;
+                        userMenuSelection = (Menu)userChoice;
                     }
 
-                    
+
+                    //switch will add user choice to the list for their current order 
+                    switch (userMenuSelection)
+                    {
+                        case Menu.menuItem1:
+                            break;
+                        case Menu.menuItem2:
+                            break;
+                        case Menu.menuItem3:
+                            break;
+                        case Menu.menuItem4:
+                            break;
+                        case Menu.menuItem5:
+                            break;
+                        case Menu.menuItem6:
+                            break;
+                        case Menu.menuItem7:
+                            break;
+                        case Menu.menuItem8:
+                            break;
+                        case Menu.menuItem9:
+                            break;
+                        case Menu.menuItem10:
+                            break;
+                        case Menu.menuItem11:
+                            break;
+                        case Menu.menuItem12:
+                            break;
+                        default:
+                            break;
+                    }
+
+
 
 
                 }
 
-
-
-
-                TryAgain:
+            TryAgain:
                 Console.WriteLine("Would you like to repeat the program?");
                 Console.Write("Enter y to coninue or anything else to quit: ");
                 userInput = Console.ReadLine();
@@ -89,12 +118,9 @@ namespace GC_AHBC_midterm_ZT_SS_AC
                     runProgramAgain = false;
                 }
 
-
-
-
             } while (runProgramAgain);
 
-            Exit:
+        Exit:
             Console.WriteLine("Thank you for shopping at Jitters Coffee House!");
             Console.WriteLine("Please press any key to continue: ");
             Console.ReadKey();
