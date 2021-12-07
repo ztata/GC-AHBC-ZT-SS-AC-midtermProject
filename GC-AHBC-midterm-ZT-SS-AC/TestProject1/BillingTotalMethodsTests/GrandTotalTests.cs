27bc8ecc44586f023b15cef59356a550c81d24ba
Steve Schaner <schanerst@gmail.com>
2021-12-06 21:40:17 -0500

    Created separate files for testing BillingTotal methods. Started FileHelper testing.

---
+++ GC-AHBC-midterm-ZT-SS-AC/TestProject1/BillingTotalMethodsTests/GrandTotalTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace GC_AHBC_midterm_ZT_SS_AC.Tests.BillingTotalMethodsTests
+{
+    public class GrandTotalTests
+    {
+        // Happy path
+        [Fact]
+        public static void ProvideCurrentOrderList_ReturnGrandTotal()
+        {
+            // Arrange
+            List<Product> currentOrderList = new List<Product>();
+            Product product1 = new Product();
+            product1.Name = "Caffe Americano";
+            product1.Price = 3.95;
+            product1.Category = "Hot Coffee";
+            product1.Description = "Water, Brewed Espresso";
+
+            Product product2 = new Product();
+            product2.Name = "Caffe Americano";
+            product2.Price = 3.95;
+            product2.Category = "Hot Coffee";
+            product2.Description = "Water, Brewed Espresso";
+
+            currentOrderList.Add(product1);
+            currentOrderList.Add(product2);
+
+            // Act
+            double subtotal = BillingTotal.SubTotal(currentOrderList);
+            double subtotalSalesTax = BillingTotal.SalesTax(subtotal, .06);
+            double returnValue = BillingTotal.GrandTotal(subtotal, subtotalSalesTax);
+
+            // Assert
+            Assert.Equal(8.37, returnValue);
+        }
+
+    }
+}
---
+++ GC-AHBC-midterm-ZT-SS-AC/TestProject1/BillingTotalMethodsTests/SalesTaxTests.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace GC_AHBC_midterm_ZT_SS_AC.Tests.BillingTotalMethodsTests
+{
+    public class SalesTaxTests
+    {
+        // Happy path
+        [Fact]
+        public static void ProvideCurrentOrderList_ReturnSalesTax()
+        {
+            // Arrange
+            List<Product> currentOrderList = new List<Product>();
+            Product product1 = new Product();
+            product1.Name = "Caffe Americano";
+            product1.Price = 3.95;
+            product1.Category = "Hot Coffee";
+            product1.Description = "Water, Brewed Espresso";
+
+            Product product2 = new Product();
+            product2.Name = "Caffe Americano";
+            product2.Price = 3.95;
+            product2.Category = "Hot Coffee";
+            product2.Description = "Water, Brewed Espresso";
+
+            currentOrderList.Add(product1);
+            currentOrderList.Add(product2);
+
+            // Act
+            double subtotal = BillingTotal.SubTotal(currentOrderList);
+            double returnValue = BillingTotal.SalesTax(subtotal, .06);
+
+            // Assert
+            Assert.Equal(0.47, returnValue);
+        }
+
+        // Happy path
+        [Fact]
+        public static void ProvideCurrentOrderList_ReturnGrandTotal()
+        {
+            // Arrange
+            List<Product> currentOrderList = new List<Product>();
+            Product product1 = new Product();
+            product1.Name = "Caffe Americano";
+            product1.Price = 3.95;
+            product1.Category = "Hot Coffee";
+            product1.Description = "Water, Brewed Espresso";
+
+            Product product2 = new Product();
+            product2.Name = "Caffe Americano";
+            product2.Price = 3.95;
+            product2.Category = "Hot Coffee";
+            product2.Description = "Water, Brewed Espresso";
+
+            currentOrderList.Add(product1);
+            currentOrderList.Add(product2);
+
+            // Act
+            double subtotal = BillingTotal.SubTotal(currentOrderList);
+            double subtotalSalesTax = BillingTotal.SalesTax(subtotal, .06);
+            double returnValue = BillingTotal.GrandTotal(subtotal, subtotalSalesTax);
+
+            // Assert
+            Assert.Equal(8.37, returnValue);
+        }
+
+    }
+}
