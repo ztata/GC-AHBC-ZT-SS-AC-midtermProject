9b43d1fd4313e433ee94d1d0c4b51b4affcb02a9
ztata <zacharyptata@gmail.com>
2021-12-06 20:45:15 -0500

    updated expiration date validation method and added unit tests for the billing regex class

---
+++ GC-AHBC-midterm-ZT-SS-AC/TestProject1/BillingRegexMethodsTests/ValidateCardNumberTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using GC_AHBC_midterm_ZT_SS_AC;
+using Xunit;
+
+namespace GC_AHBC_midterm_ZT_SS_AC.Tests.BillingRegexMethodsTests
+{
+    public class ValidateCardNumberTests
+    {
+        [Theory]
+        [InlineData("1234-1234-1234-1234", true)]
+        [InlineData("1111-1111-1111-1111", true)]
+        [InlineData("0000-0000-0000-0000", true)]
+        [InlineData("9999-9999-9999-9999", true)]
+        [InlineData("1256-2345-5678-8989", true)]
+        public static void ProvideValidCardNumber_ReturnsTrue(string cardNumber, bool expectedResult)
+        {
+            //arrange
+
+            //act
+            bool actualReturn = BillingRegexMethods.ValidateCardNumber(cardNumber);
+            //assert
+            Assert.Equal(expectedResult, actualReturn);
+        }
+
+        [Theory]
+        [InlineData("1234123412341234", false)]
+        [InlineData("1111-1111-1111", false)]
+        [InlineData("0000-0000-00000000", false)]
+        [InlineData("9999-9999-9999-99", false)]
+        [InlineData("12546-24345-45678-48989", false)]
+        public static void ProvideInvalidCardNumber_ReturnsFalse(string cardNumber, bool expectedResult)
+        {
+            //arrange
+
+            //act
+            bool actualReturn = BillingRegexMethods.ValidateCardNumber(cardNumber);
+            //assert
+            Assert.Equal(expectedResult, actualReturn);
+        }
+    }
+
+}
